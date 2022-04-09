92a6a0a2665f0811519a4be9e29f526100ae134f
israelruiz2005 <israelruiz2005@gmail.com>
2022-04-08 22:00:10 -0300

    Ajusta setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Texto no espelho",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/israelruiz2005/texto_no_espelho_package"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
