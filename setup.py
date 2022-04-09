f5dd6691b423615afb4771e841b4b4507efc8ee3
israelruiz2005 <israelruiz2005@gmail.com>
2022-04-09 19:45:10 -0300

    Altera setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="texto_no_espelho",
-    version="0.0.1",
+    version="0.0.2",
     author="Israel Ruiz",
     author_email="israelruiz2005@gmail.com",
     description="Texto no espelho",
